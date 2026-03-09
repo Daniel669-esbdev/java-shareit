dc94ef51c66359cb8caabfdd5a2478ce955404c1
Daniel <dzdaniel669@gmail.com>
2026-03-09 22:01:44 +0500

    add-item-requests-and-gateway7

--- server/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ server/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -26,7 +26,7 @@ public class BookingController {
 
     @PatchMapping("/{bookingId}")
     public BookingDto approve(@RequestHeader("X-Sharer-User-Id") Long userId,
-        @PathVariable Long bookingId,
+        @PathVariable("bookingId") Long bookingId,
         @RequestParam(name = "approved") Boolean approved) {
         log.info("Approving booking ID: {} by user ID: {}", bookingId, userId);
         return bookingService.approve(userId, bookingId, approved);
@@ -34,21 +34,21 @@ public class BookingController {
 
     @GetMapping("/{bookingId}")
     public BookingDto getById(@RequestHeader("X-Sharer-User-Id") Long userId,
-        @PathVariable Long bookingId) {
+        @PathVariable("bookingId") Long bookingId) {
         log.info("Getting booking ID: {} for user ID: {}", bookingId, userId);
         return bookingService.getById(userId, bookingId);
     }
 
     @GetMapping
     public List<BookingDto> getAllByBooker(@RequestHeader("X-Sharer-User-Id") Long userId,
-        @RequestParam(defaultValue = "ALL") String state) {
+        @RequestParam(name = "state", defaultValue = "ALL") String state) {
         log.info("Getting all bookings for booker ID: {} with state: {}", userId, state);
         return bookingService.getAllByBooker(userId, state);
     }
 
     @GetMapping("/owner")
     public List<BookingDto> getAllByOwner(@RequestHeader("X-Sharer-User-Id") Long userId,
-        @RequestParam(defaultValue = "ALL") String state) {
+        @RequestParam(name = "state", defaultValue = "ALL") String state) {
         log.info("Getting all bookings for owner ID: {} with state: {}", userId, state);
         return bookingService.getAllByOwner(userId, state);
     }

7ef9b4cb6f8b6e7f278b0104d9023c57ba01505e
Daniel <dzdaniel669@gmail.com>
2026-03-11 00:00:49 +0500

    add-item-requests-and-gateway8

--- gateway/src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
+++ gateway/src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -30,6 +30,22 @@ class ItemRequestControllerTest {
     private MockMvc mvc;
 
     @Test
+    void create_whenValid_thenOk() throws Exception {
+        ItemRequestDto dto = ItemRequestDto.builder()
+                .description("Test description")
+                .build();
+
+        when(itemRequestClient.create(anyLong(), any(ItemRequestDto.class)))
+                .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+
+        mvc.perform(post("/requests")
+                        .header("X-Sharer-User-Id", 1L)
+                        .content(mapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
     void create_whenDescriptionBlank_thenBadRequest() throws Exception {
         ItemRequestDto dto = ItemRequestDto.builder()
                 .description("")
@@ -43,15 +59,6 @@ class ItemRequestControllerTest {
     }
 
     @Test
-    void getAllRequests_whenInvalidParams_thenBadRequest() throws Exception {
-        mvc.perform(get("/requests/all")
-                        .header("X-Sharer-User-Id", 1L)
-                        .param("from", "-1")
-                        .param("size", "0"))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
     void getUserRequests_whenValid_thenOk() throws Exception {
         when(itemRequestClient.getUserRequests(anyLong()))
                 .thenReturn(new ResponseEntity<>(HttpStatus.OK));
@@ -59,5 +66,57 @@ class ItemRequestControllerTest {
         mvc.perform(get("/requests")
                         .header("X-Sharer-User-Id", 1L))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void getAllRequests_whenValid_thenOk() throws Exception {
+        when(itemRequestClient.getAllRequests(anyLong(), anyInt(), anyInt()))
+                .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+
+        mvc.perform(get("/requests/all")
+                        .header("X-Sharer-User-Id", 1L)
+                        .param("from", "0")
+                        .param("size", "10"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getAllRequests_whenInvalidFrom_thenBadRequest() throws Exception {
+        mvc.perform(get("/requests/all")
+                        .header("X-Sharer-User-Id", 1L)
+                        .param("from", "-1")
+                        .param("size", "10"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void getAllRequests_whenInvalidSize_thenBadRequest() throws Exception {
+        mvc.perform(get("/requests/all")
+                        .header("X-Sharer-User-Id", 1L)
+                        .param("from", "0")
+                        .param("size", "0"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void getById_whenValid_thenOk() throws Exception {
+        when(itemRequestClient.getRequestById(anyLong(), anyLong()))
+                .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+
+        mvc.perform(get("/requests/1")
+                        .header("X-Sharer-User-Id", 1L))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void create_whenNoUserHeader_thenBadRequest() throws Exception {
+        ItemRequestDto dto = ItemRequestDto.builder()
+                .description("Test")
+                .build();
+
+        mvc.perform(post("/requests")
+                        .content(mapper.writeValueAsString(dto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 }

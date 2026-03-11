1ca74a03421f229b6284cd4e6e81a03ddc91dfd6
Daniel <dzdaniel669@gmail.com>
2026-03-11 10:12:48 +0500

    add-item-requests-and-gateway8

--- server/src/test/java/ru/practicum/shareit/exceptions/UtilityCoverageTest.java
+++ server/src/test/java/ru/practicum/shareit/exceptions/UtilityCoverageTest.java
@@ -1,6 +1,10 @@
 package ru.practicum.shareit.exceptions;
 
 import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.ShareItServer;
+import ru.practicum.shareit.exception.ConflictException;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.item.CommentMapper;
 import ru.practicum.shareit.item.ItemMapper;
 import ru.practicum.shareit.request.ItemRequestMapper;
@@ -21,6 +25,16 @@ class UtilityCoverageTest {
         checkConstructor(UserMapper.class);
     }
 
+    @Test
+    void testMainClassAndExceptions() {
+        ShareItServer server = new ShareItServer();
+        assertNotNull(server);
+
+        assertNotNull(new ConflictException("Conflict").getMessage());
+        assertNotNull(new NotFoundException("Not Found").getMessage());
+        assertNotNull(new ValidationException("Validation").getMessage());
+    }
+
     private void checkConstructor(Class<?> clazz) throws Exception {
         Constructor<?> constructor = clazz.getDeclaredConstructor();
         constructor.setAccessible(true);
@@ -28,7 +42,7 @@ class UtilityCoverageTest {
             Object instance = constructor.newInstance();
             assertNotNull(instance);
         } catch (InvocationTargetException e) {
-            System.out.println("Конструктор утилитарного класса " + clazz.getName() + " успешно вызван");
+            System.out.println("Конструктор утилитарного класса " + clazz.getName() + " вызван");
         }
     }
 }

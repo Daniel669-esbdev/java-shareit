f75f9efbacdf2ee99f3f653f2fadd4be2bae27c4
Daniel <dzdaniel669@gmail.com>
2026-03-11 12:05:51 +0500

    add-item-requests-and-gateway8

--- server/src/test/java/ru/practicum/shareit/item/repository/ItemRepositoryTest.java
+++ server/src/test/java/ru/practicum/shareit/item/repository/ItemRepositoryTest.java
@@ -112,4 +112,52 @@ class ItemRepositoryTest {
 
         assertThat(result).isEmpty();
     }
+
+    @Test
+    void findAllByRequest_Id() {
+        User owner = User.builder().name("Owner").email("owner@mail.com").build();
+        em.persist(owner);
+
+        ru.practicum.shareit.request.ItemRequest request = ru.practicum.shareit.request.ItemRequest.builder()
+                .description("Нужна вещь")
+                .created(java.time.LocalDateTime.now())
+                .build();
+        em.persist(request);
+
+        Item item = Item.builder()
+                .name("Вещь")
+                .description("Описание")
+                .available(true)
+                .owner(owner)
+                .request(request)
+                .build();
+        em.persist(item);
+
+        List<Item> result = itemRepository.findAllByRequest_Id(request.getId());
+
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0).getRequest().getId()).isEqualTo(request.getId());
+    }
+
+    @Test
+    void findAllByRequest_IdIn() {
+        User owner = User.builder().name("Owner").email("owner2@mail.com").build();
+        em.persist(owner);
+
+        ru.practicum.shareit.request.ItemRequest req1 = ru.practicum.shareit.request.ItemRequest.builder()
+                .description("Запрос 1").created(java.time.LocalDateTime.now()).build();
+        ru.practicum.shareit.request.ItemRequest req2 = ru.practicum.shareit.request.ItemRequest.builder()
+                .description("Запрос 2").created(java.time.LocalDateTime.now()).build();
+        em.persist(req1);
+        em.persist(req2);
+
+        Item item1 = Item.builder().name("I1").available(true).owner(owner).request(req1).build();
+        Item item2 = Item.builder().name("I2").available(true).owner(owner).request(req2).build();
+        em.persist(item1);
+        em.persist(item2);
+
+        List<Item> result = itemRepository.findAllByRequest_IdIn(List.of(req1.getId(), req2.getId()));
+
+        assertThat(result).hasSize(2);
+    }
 }
